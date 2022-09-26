05524c95cc794905fb296a3bfbfb6239589f2534
cwxvxn <clavenrl@gmail.com>
2022-09-26 00:28:21 -0400

    save/load state, leave, forfeit, clear

--- CowBot.py
+++ CowBot.py
@@ -1,5 +1,7 @@
+import pickle
 import interactions
 import aiocron
+import os
 
 from datetime import datetime
 from os import environ
@@ -14,14 +16,6 @@ roleID = 1023740823625543720
 
 bot = interactions.Client(token=token)
 
-
-# Index 0 represents current QM
-# Index 1+ are users queued for QM
-# Users cannot queue while they are already queue'd
-# QM cannot queue until their reign has ended
-# TODO: load from DB or file on init
-# TODO: store to DB or file on change
-qotdQueue = []
 
 @bot.event
 async def on_ready():
@@ -41,6 +35,28 @@ async def on_ready():
         interactions.Channel,
         object_id=channelID
     )
+
+    # Index 0 represents current QM
+    # Index 1+ are users queued for QM
+    # Users cannot queue while they are already queue'd
+    # QM cannot queue until their reign has ended
+    global qotdQueue
+    if os.path.exists('queue.pkl'):
+        try:
+            qotdQueue = await loadState()
+            log(f"Loaded queue: {qotdQueue}")
+            await removeALlQMRoles()
+            if qotdQueue:
+                await giveQM(qotdQueue[0])
+                await channel.send(f"I have been restarted and recovered data.")
+                await statusHelper(channel)
+        except Exception as e:
+            log("Error loading state: " + repr(e))
+            await clearHelper()
+    else:
+        await clearHelper()
+    
+    
 
 ###################################
 #              QUEUE              #
@@ -64,11 +80,51 @@ async def queue(ctx: interactions.CommandContext):
         await giveQM(member)
         qotdQueue.append(member)
         await ctx.send(f"Congrats {member.mention}, you are now the Question Master!")
-        return
+    else:
+        qotdQueue.append(member)
+        await ctx.send(f"You have joined the queue at position **{len(qotdQueue) - 1}**.")
+        #TODO: send the time that their question master reign will start
     
-    qotdQueue.append(member)
-    await ctx.send(f"You have joined the queue at position **{len(qotdQueue) - 1}**.")
-    #TODO: send the time that their question master reign will start
+    saveState()
+
+
+###################################
+#             Leave               #
+###################################
+@bot.command(
+    name='leave',
+    description="Remove yourself from the queue"
+)
+async def leave(ctx: interactions.CommandContext):
+    member = ctx.author
+    log(f"Leave({member})")
+
+    if qotdQueue.count(member):
+        if qotdQueue.index(member):
+            qotdQueue.pop(qotdQueue.index(member))
+            saveState()
+            await ctx.send("You've successfully left the queue.")
+        else:
+            await ctx.send("You're the Question Master, dummy!\nDon't use leave, use /forfeit if you want forfeit the rest of your reign.")
+        
+    else:
+        await ctx.send("You can't leave the queue if you aren't in it, dummy!")
+
+
+###################################
+#            Forfeit              #
+###################################
+@bot.command(
+    name='forfeit',
+    description="Forfeit the remainder of your Question Master reign"
+)
+async def forfeit(ctx: interactions.CommandContext):
+    member = ctx.author
+    log(f"Forfeit({member})")
+    if qotdQueue and qotdQueue[0] == member:
+        await cycleHelper(ctx)
+    else:
+        await ctx.send("The forfeit command is only for Question Masters, dummy!")
 
 
 ###################################
@@ -81,17 +137,19 @@ async def queue(ctx: interactions.CommandContext):
 async def status(ctx: interactions.CommandContext):
     member = ctx.author
     log(f"Status({member})")
+    await statusHelper(ctx)
 
+async def statusHelper(receiver):
     if qotdQueue:
         msg = f"The current Question Master is **{qotdQueue[0]}**.\n"
         if qotdQueue[1:]:
-            msg += f"Queued Members: {list(map(lambda member: member.name, qotdQueue[1:]))}"
+                msg += f"Queued Members: {[user.name for user in qotdQueue[1:]]}"
         else:
             msg += "There are no users in the queue."
-        await ctx.send(msg)
+        await receiver.send(msg)
         return
     
-    await ctx.send("There is no current Question Master, the queue is empty.\nUse /queue to become the Question Master right now!")
+    await receiver.send("There is no current Question Master, the queue is empty.\nUse /queue to become the Question Master right now!")
 
 
 
@@ -103,7 +161,7 @@ async def status(ctx: interactions.CommandContext):
     description="Manually cycle the Question Master",
     default_member_permissions=interactions.Permissions.ADMINISTRATOR
 )
-async def cycleQM(ctx: interactions.CommandContext):
+async def manualCycle(ctx: interactions.CommandContext):
     log(f"Cycle({ctx.author})")
     status = await cycleHelper(ctx)
     if status == -1:
@@ -112,31 +170,64 @@ async def cycleQM(ctx: interactions.CommandContext):
 async def cycleHelper(receiver):
     if qotdQueue:
         oldQM = qotdQueue.pop(0)
-        await oldQM.remove_role(qmRole, guildID)
+        await removeQM(oldQM)
         
         if qotdQueue:
             newQM = qotdQueue[0]
             await giveQM(newQM)
             await receiver.send(f"Question Master has been cycled: **{oldQM.name}** has been dethroned.\nCongrats {newQM.mention}, you are now the Question Master!")
-            return 1
         else:
             await receiver.send(f"Question Master has been cycled: **{oldQM.name}** has been dethroned.\nThere is no new Question Master.")
-            return 0
-    return -1
+        
+        saveState()
+        return 0
+    else:
+        return -1
 
 # Trigger Cycle every day at Midnight
 @aiocron.crontab('0 0 * * *')
-async def triggerCycle():
-    log("Cycle Triggered")
+async def autoCycle():
+    log("AutoTriggering Cycle")
     status = await cycleHelper(channel)
     if status == -1:
         log("AutoTriggered Cycle: No effect.")
-    log(f"AutoTriggered Cycle: case({status}).")
+    else:
+        log(f"AutoTriggered Cycle: case({status}).")
+
+
+###################################
+#             CLEAR               #
+###################################
+@bot.command(
+    name='clear',
+    description="Manually clear all Question Master related data",
+    default_member_permissions=interactions.Permissions.ADMINISTRATOR
+)
+async def clear(ctx: interactions.CommandContext):
+    log(f"Clear({ctx.author})")
+    await clearHelper()
+    await ctx.send("Clear was successful")
+
+async def clearHelper():
+    await removeALlQMRoles()
+    qotdQueue = []
+    saveState()
+
+
 
 
 ###################################
 #        HELPER FUNCTIONS         #
 ###################################
+
+# Clears the QM role from everyone in the server
+async def removeALlQMRoles():
+    guild = await interactions.get(
+        bot,
+        interactions.Guild,
+        object_id=guildID
+    )
+    [await removeQM(member) for member in guild.members]
 
 
 # Give QM role to Member
@@ -146,6 +237,32 @@ async def giveQM(member: interactions.api.models.Member):
     except interactions.api.error.LibraryException as e:
         log(f"Error giving qm role: LibraryException: {e}")
 
+# Remove QM role from Member
+async def removeQM(member: interactions.api.models.Member):
+    try:
+        await member.remove_role(qmRole, guildID)
+    except interactions.api.error.LibraryException as e:
+        log(f"Error removing qm role: LibraryException: {e}")
+
+def saveState():
+    idList = [int(user.id) for user in qotdQueue]
+    log('saving state, ids: ' + str(idList))
+    with open('queue.pkl', 'wb') as f:
+        pickle.dump(idList, f)
+
+async def loadState():
+    with open('queue.pkl', 'rb') as f:
+        idList = pickle.load(f)
+        log('Loaded state, ids: ' + str(idList))
+        return [await idToMember(id) for id in idList]
+
+async def idToMember(id):
+    return await interactions.get(
+        bot,
+        interactions.Member,
+        object_id=id,
+        parent_id=guildID
+    )
 
 def log(msg):
     print(f"{datetime.now()} | {msg}")
